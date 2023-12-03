f6bd4274629c14e0adeb79d00f9076c9f3348eb4
imagozxd <144135582+imagozxd@users.noreply.github.com>
2023-12-03 02:16:39 -0500

    yanoaguantoyanoaguanto

--- Assets/Scripts/Fish.cs
+++ Assets/Scripts/Fish.cs
@@ -1,12 +1,12 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Wood : MonoBehaviour
+public class Pescado : MonoBehaviour
 {
     private Rigidbody rb;
+
     public float forceHeight; // salto al aparecer
-    public float rotationSpeed; // velocidad de rotacion
+    
     public float gravity = 9.8f;
 
     private bool isElevating = true;
@@ -16,29 +16,30 @@ public class Wood : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         rb.velocity = Vector3.up * forceHeight;
     }
+
     private void Update()
     {
         if (isElevating)
         {
             //aplicar gravedad
-            rb.velocity = rb.velocity + Vector3.down* gravity * Time.deltaTime;
+            rb.velocity = rb.velocity + Vector3.down * gravity * Time.deltaTime;
 
-            transform.Rotate(Vector3.up, Random.Range(-rotationSpeed, rotationSpeed) * Time.deltaTime);
+            
         }
         else
         {
             // llenar por si se me ocurre algo xd
         }
-        
     }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("terrain_tag"))
         {
-           isElevating = false;
-           //rb.isKinematic = false; // ojito aqui 
-            Debug.Log("se detiene isElevating, deberia detenerse la madera en el suelo");
+            isElevating = false;
+            //rb.isKinematic = false;
+            Debug.Log("Se detiene el salto");
         }
-        
     }
 }
+
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     private Rigidbody rb;
     private Vector3 velocity; // Vector de velocidad privado
+    private Animator animator;
 
     public int damage;
 
@@ -16,13 +17,36 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         rb.velocity = velocity;
+
+        // Verificamos si animator no es null antes de intentar acceder a él
+        if (animator != null)
+        {
+            // Verificamos si hay movimiento para activar/desactivar la animación
+            if (Keyboard.current.sKey.isPressed)
+            {
+                // Activamos la animación "Walking" si hay movimiento
+                animator.SetBool("IsWalking", true);
+                Debug.Log("Se está reproduciendo la animación");
+            }
+            else
+            {
+                // Desactivamos la animación si no hay movimiento
+                animator.SetBool("IsWalking", false);
+            }
+        }
+        Debug.Log("Animator: " + animator);
+        if (animator != null)
+        {
+            // Resto del código
+        }
     }
 
 
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 movementInput = context.ReadValue<Vector2>();
-        velocity = new Vector3(movementInput.x * speed,0f, movementInput.y * speed);        
+        velocity = new Vector3(movementInput.x * speed,0f, movementInput.y * speed);
+        Debug.Log("Movimiento detectado: " + velocity);
     }
     public void OnPressJ(InputAction.CallbackContext context)
     {
