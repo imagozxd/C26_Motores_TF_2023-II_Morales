ff10335f42d0c20bc3747ce046156759b4ea8102
imagozxd <144135582+imagozxd@users.noreply.github.com>
2023-12-13 01:05:13 -0500

    arbol1-profe0

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,22 +6,26 @@ public class Weapon : MonoBehaviour
 {    
     public int cantidadDanio = 10;
     public int danioReal;
+    
+    public Arbol arbol; 
    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("arbol")) 
         {
             danioReal = Random.Range((cantidadDanio /2)+Random.Range(0,10), cantidadDanio + Random.Range(0, 10));
-            //Arbol arbol = (other.gameObject.GetComponent<Arbol>() this);
+            arbol = other.GetComponent<Arbol>();
+
             
-            //if (arbol != null)
-            //{
-            //    arbol.ActualizarVida(danioReal);
-            //    Debug.Log("vida del arbol objetivo: " + arbol.vidaActualArbol);
-            //}
+            if (arbol != null)
+            {
+                Debug.Log("ASDSADASDA");
+                arbol.ActualizarVida(danioReal);
+               Debug.Log("vida del arbol objetivo: " + arbol.vidaActualArbol);
+            }
             
 
-            //Debug.Log("Desde weapon: " + danioReal);
+            Debug.Log("Desde weapon: " + danioReal);
         }
     }
 
